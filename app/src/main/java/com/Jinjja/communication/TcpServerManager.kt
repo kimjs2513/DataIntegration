052283d0170589760a7d461bfbe278a35751e27b
jinseong <kimjs2513@gmail.com>
2022-02-25 10:58:52 +0900

    tcp/ip client

--- app/src/main/java/com/Jinjja/communication/TcpServerManager.kt
+++ app/src/main/java/com/Jinjja/communication/TcpServerManager.kt
@@ -1,16 +1,26 @@
 package com.Jinjja.communication
+import android.os.Handler
+import android.util.Log
 import com.Jinjja.databinding.ActivityMainBinding
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.lang.Exception
 import java.net.Socket
+import com.Jinjja.service.DataServerService
 
 class TcpServerManager {
+
+
     private lateinit var binding: ActivityMainBinding
+
+
 
     inner class TcpServerThread: Thread(){
         override fun run() {
             try{
+                
+                Log.d("test1", "쓰래드 시작")
+                
                 var socket = Socket("192.168.0.100", 55555)
 
                 var input = socket.getInputStream()
@@ -27,7 +37,6 @@ class TcpServerManager {
 
                 socket.close()
 
-                binding.cameraDataText.text = data1.toString()
 
 
             }catch (e:Exception){
--- app/src/main/java/com/Jinjja/service/DataServerService.kt
+++ app/src/main/java/com/Jinjja/service/DataServerService.kt
@@ -2,14 +2,16 @@ package com.Jinjja.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
 import android.os.SystemClock
+import android.util.Log
 import com.Jinjja.databinding.ActivityMainBinding
 import com.Jinjja.communication.TcpServerManager
 
 class DataServerService : Service() {
     private lateinit var binding: ActivityMainBinding
-    var isRunning = false
+
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -17,17 +19,17 @@ class DataServerService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-        isRunning = true
-        while(isRunning){
-            SystemClock.sleep(1000)
-            TcpServerManager().TcpServerThread().start()
-        }
+        Log.d("test1", "서비스 시작 됨")
+
+
+        var TcpThread = TcpServerManager().TcpServerThread()
+        TcpThread.start()
 
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
-        isRunning = false
+        Log.d("test1", "서비스 종료 됨")
         super.onDestroy()
     }
 
