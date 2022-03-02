b55b880762cc853138e6cb5568b3737edc17e191
jinseong <kimjs2513@gmail.com>
2022-03-02 19:32:42 +0900

    client upload 2

--- app/src/main/java/com/Jinjja/communication/TcpServerManager.kt
+++ app/src/main/java/com/Jinjja/communication/TcpServerManager.kt
@@ -1,20 +1,14 @@
 package com.Jinjja.communication
-import android.os.Handler
+
 import android.os.SystemClock
 import android.util.Log
-import com.Jinjja.databinding.ActivityMainBinding
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.lang.Exception
 import java.net.Socket
-import com.Jinjja.service.DataServerService
 
 class TcpServerManager {
 
-
-    private lateinit var binding: ActivityMainBinding
-
-    var isRunnig = true
 
     var value = 60
 
